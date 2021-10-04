fdfbe014362261ca7316464b61f00f8a460c599e
Nazihah Ahwan <nazihah.ahwan8992@gmail.com>
2021-10-04 15:14:42 +0800

    study-list-screen

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,11 +1,36 @@
 import React from "react";
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 const ListScreen = () =>
 {
-    return <Text>List Screen</Text>;
+    const friends =[
+        { name: 'Friend #1', age: 20},
+        { name: 'Friend #2', age: 21},
+        { name: 'Friend #3', age: 22},
+        { name: 'Friend #4', age: 23},
+        { name: 'Friend #5', age: 24},
+        { name: 'Friend #6', age: 25},
+        { name: 'Friend #7', age: 26},
+        { name: 'Friend #8', age: 27},
+        { name: 'Friend #9', age: 28}
+    ];
+    return(
+        <FlatList 
+        keyExtractor ={(friend)=> friend.name}
+        data= {friends} 
+        renderItem={({item})=>{
+            return <Text style={styles.textStyle}>
+                    {item.name}- Age {item.age}</Text>
+        }}>
+        </FlatList>
+    ); 
 };
 
-const style = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textStyle:{
+        marginVertical: 50
+
+    }
+});
 
 export default ListScreen;
