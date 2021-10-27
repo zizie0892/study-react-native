2a326e85a5b14a58122946e2d102970042eb7d99
Nazihah Ahwan <nazihah.ahwan8992@gmail.com>
2021-10-27 10:40:22 +0800

    State Management in React Component Exp 3

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,12 +1,12 @@
 import React from "react";
 import {View, Text, StyleSheet, Button} from 'react-native';
 
-const ColorCounter = () => {
+const ColorCounter = ({color, onIncrease, onDecrease}) => {
     return (
     <View>
-        <Text>Red</Text>
-        <Button title="Increase Red" />
-        <Button title="Descrease Red" />
+        <Text>{color}</Text>
+        <Button onPress={() => onIncrease()} title={`Increase ${color}`} />
+        <Button onPress={() => onDecrease()} title={`Decrease ${color}`} />
     </View>
     );
 };
