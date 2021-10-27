d6e844b08bc16f06f15e77735ed7cd37e0a77eae
Nazihah Ahwan <nazihah.ahwan8992@gmail.com>
2021-10-27 16:50:49 +0800

    Handling Screen Layout

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -34,6 +34,10 @@ const HomeScreen = ({navigation})=> {
         onPress = {() => navigation.navigate('Text')}
         title="Go to Text Demo" 
       />
+      <Button 
+        onPress = {() => navigation.navigate('Box')}
+        title="Go to Box Demo" 
+      />
   </View>
   );
 };
