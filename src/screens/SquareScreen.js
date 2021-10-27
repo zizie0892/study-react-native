3e2e99d2c7b3799be1cf679c61b1218231692639
Nazihah Ahwan <nazihah.ahwan8992@gmail.com>
2021-10-27 10:55:01 +0800

    Restoring Validation

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -8,12 +8,14 @@ const reducer = (state, action) => {
     //action === {colorTochange: 'red' || 'green' || 'blue', amount: 15 || -15}
     switch(action.colorToChange){
         case 'red':
-            //never going to do: state.red
-            return {...state, red:state.red + action.amount};
+            return state.red + action.amount > 255 || state.red +  action.amount < 0
+                ? state: {...state, red:state.red + action.amount};
         case 'green':
-            return {...state, green:state.green + action.amount};
+            return state.green + action.amount > 255 || state.green +  action.amount < 0
+                ? state: {...state, green:state.green + action.amount};
         case 'blue':
-            return {...state, blue:state.blue + action.amount};
+            return state.blue + action.amount > 255 || state.blue +  action.amount < 0
+                ? state: {...state, blue:state.blue + action.amount};
         default:
             return state;
     }
